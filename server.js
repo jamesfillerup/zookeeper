e1dd414c654d1fe92e9ad6559c65baf7a090bef0
jamesfill <jamesp.fillerup@gmail.com>
2022-04-06 19:20:55 -0600

    something fishy

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
+const exp = require('constants');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));// this adds the css and javascript to the html page
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -84,6 +86,8 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+
+
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
@@ -95,6 +99,23 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });  
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
